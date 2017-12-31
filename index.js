6424c6ab7465909e050f7b3ead5a8ffa9258017d
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2017-12-31 21:57:00 +0800

    Added a copy of index.js on root directory

---
+++ index.js
@@ -0,0 +1,61 @@
+
+var cool = require('cool-ascii-faces');
+
+const path = require('path');
+
+const express = require('express');
+const app = express();
+      app.use(express.static('public'));
+      app.use(express.static(path.join(__dirname,'public')));
+
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
+server.listen(process.env.PORT);
+
+
+app.get('/', (req, res)=> {
+    res.render('index');
+});
+
+
+
+let people = {};
+let peopleCount = 0;
+
+
+io.on('connection', (socket)=> {
+
+    // JOINING THE AWESOME CHAT
+    socket.on('join', (name) => {
+    
+      socket.broadcast.emit('join', name);
+      people[socket.id] = name;
+      peopleCount++;
+      io.emit('update',`There are <span style="color: green;">${peopleCount}</span> people online.`);
+
+
+    });
+    
+    // SENDING OF MESSAGE
+    socket.on('chatMessage', (message) => {
+       socket.broadcast.emit('chatMessage',`<strong>${people[socket.id]}: </strong> ${message}`); 
+    })
+
+
+    // DISCONNECTING FROM AWESOME CHAT :(
+    socket.on('disconnect', (name) => {
+        
+        console.log(`${people[socket.id]} has disconnected.`);  
+        io.emit('disconnect', people[socket.id]);
+        delete people[socket.id];
+        peopleCount--;
+        io.emit('update',`There are <span style="color: green;">${peopleCount}</span> people online.`);
+
+    });
+
+});
+
+
+
+
