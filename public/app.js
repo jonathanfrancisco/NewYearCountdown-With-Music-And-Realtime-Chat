beec5f6ebec366b57d911bf9c26406dd388af447
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2018-01-04 00:07:22 +0800

    Refactored

--- public/app.js
+++ public/app.js
@@ -1,26 +1,25 @@
 
+
+/*********************
+        COUNT DOWN
+ * ***************** */
 const countDownDate = new Date('January 1, 2018 00:00:00').getTime();
 
 const countDown = () => {
     
     let dateNow = new Date().getTime();
     let remainingTime = countDownDate - dateNow;
-
     let days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
     let hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
-    
     let countdownText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-
-
 
     if(countdownText === '0d 0h 1m 8s') {
         const auldLangSyne = document.querySelector('audio');
         auldLangSyne.play();
     }
     
-
     document.querySelector('#count-down h1').innerHTML = countdownText;
 
     if(countdownText === '0d 0h 0m 11s')
@@ -37,14 +36,11 @@ const countDown = () => {
             document.querySelector('#count-down').style.color = `rgb(${r},${g},${b})`;
         },200);
 
-
     }
-
-
 
 }
 
-var interval = setInterval(countDown, 1000);
+let interval = setInterval(countDown, 1000);
 
 
 
@@ -56,70 +52,38 @@ const socket = io();
 const form = document.querySelector('#chat-pane form');
 let name = undefined;
 
+function appendMessage(message) {
+    const messages = document.querySelector('#messages');
+    const li = document.createElement('li');
+          li.innerHTML = message;
+    messages.appendChild(li);
+}
+
 form.addEventListener('submit', (event) => {
     
     event.preventDefault();
 
-    let inputElement = document.querySelector('#chat-pane form input');
+    const inputElement = document.querySelector('#chat-pane form input');
     let message = inputElement.value;
     socket.emit('chatMessage',message);
 
-    const messages = document.querySelector('#messages');
-    const li = document.createElement('li');
-          li.innerHTML = `<strong id="you">${name}: </strong>${message}`;
-    messages.appendChild(li);
-
-
+    appendMessage(`<strong id="you">${name}: </strong>${message}`);
     inputElement.value = '';
 
 });
 
 socket.on('chatMessage', (message) => {
-    const messages = document.querySelector('#messages');
-    const li = document.createElement('li');
-          li.innerHTML = `${message}`;
-
-    messages.appendChild(li);
-
-    messages.scrollTop = messages.scrollHeight;
-
-    
-
+    appendMessage(message);
 });
 
 
-(function() {
-
-    name = prompt("Enter your desired name: ");
-    
-    if(name === undefined || name === null || name === '')
-        name = 'Anonymous';
-   
-    socket.emit('join', name);
-
-    const messages = document.querySelector('#messages');
-    const li = document.createElement('li');
-          li.innerHTML = '<span>You may refresh the page if you want to change your name</span>';
-    messages.appendChild(li);
-          li.innerHTML = `<span>You have joined as "${name}" in this New Year Countdown Chatroom by jonathan da awesome ! ^_^</span>`;
-    messages.appendChild(li);
-
-})();
-
-
 socket.on('join', (name) => {
-    const messages = document.querySelector('#messages');
-    const li = document.createElement('li');
-          li.innerHTML = `<span>${name} has joined the new year countdown chatroom.</span>`;
-    messages.appendChild(li);
+    appendMessage(`<span>${name} has joined the new year countdown chatroom.</span>`);
 });
 
 
 socket.on('disconnect', (name) => {
-    const messages = document.querySelector('#messages');
-    const li = document.createElement('li');
-          li.innerHTML = `${name} has disconnected.`;
-    messages.appendChild(li);
+    appendMessage(`${name} has disconnected.`);
 });
 
 socket.on('update', (status) => {
@@ -127,15 +91,19 @@ socket.on('update', (status) => {
 });
 
 
+(function() {
 
+    name = prompt("Enter your alias name: ");
+    
+    if(name === undefined || name === null || name === '')
+        name = 'Anonymous';
+   
+    socket.emit('join', name);
 
+    appendMessage('<span>You may refresh the page if you want to change your name</span>');
+    appendMessage(`<span>You have joined as "${name}" in this New Year Countdown Chatroom by jonathan da awesome ! ^_^</span>`);
 
-
-
-
-
-
-
+})();
 
 
 particlesJS.load('particles-js', 'particlesjs-config.json', function() {
